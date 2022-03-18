560c508798128908f6b705ecec98646b50f496f0
Leonard Norblad <norblad88@gmail.com>
2022-03-18 13:44:19 +0100

    Uppdaterar funktionen

--- Stock_functions.R
+++ Stock_functions.R
@@ -3,8 +3,8 @@ require("tidyverse")
 require("lubridate")
 
 # Skapar en funktion som beräknar vad som krävs för att få tillbaka värdet vid en börskrash
-value_func<-function(value_before, # Värdet innan krash
-                     value_now, # Värdet efter krash
+value_func<-function(value_before,           # Värdet innan krash
+                     value_now,              # Värdet efter krash
                      ang_stock_ret_percent){ # Snittuppgång på börsen
   # Om differensen mellan värdet innan och värdet nu är mindre än 0 har man inte gjort en förlust
   if (value_before-value_now<0){
@@ -29,8 +29,8 @@ value_func<-function(value_before, # Värdet innan krash
 }
 
 # Exempel på värden som kan matas in
-value_func(value_before = 10000, # 10 000 kr innan krash
-           value_now = 6000, # 6 000 kr efter krash
+value_func(value_before = 10000,   # 10 000 kr innan krash
+           value_now = 6000,       # 6 000 kr efter krash
            ang_stock_ret_percent=7 # Med en snittuppgång på 7% per år
 )
 
