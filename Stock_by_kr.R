e48583409013823ec9232f2866460fdae3e6626b
Leonard Norblad <norblad88@gmail.com>
2022-06-10 12:39:51 +0200

    Påbörjar funktion

---
+++ Stock_by_kr.R
@@ -0,0 +1,21 @@
+
+#install.packages("quantmod")
+#install.packages("priceR")
+library(priceR)
+library(quantmod)
+
+
+start_date <- ymd('2020-01-01')
+end_date <- today()
+
+getSymbols("TSLA", from = start_date,
+           to = end_date,warnings = FALSE,
+           auto.assign = TRUE)
+close_price <- TSLA$TSLA.Close
+dollar_price <- historical_exchange_rates(start_date = start_date,
+                          end_date = end_date,
+                          from = "USD",
+                          to = "SEK")
+tail(close_price)
+tail(dollar_price)
+sek_price <- close_price$TSLA.Close*dollar_price$one_USD_equivalent_to_x_SEK
